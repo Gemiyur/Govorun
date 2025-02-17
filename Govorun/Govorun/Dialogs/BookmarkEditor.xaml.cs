41f45a49e919c9adb578d6a39893de018aea6c1b
Геннадий <gemiyur@live.ru>
2025-02-17 17:49:35 +0300

    Редактор закладки.

---
+++ Govorun/Govorun/Dialogs/BookmarkEditor.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Govorun.Dialogs
+{
+    /// <summary>
+    /// Класс окна редактора закладки книги.
+    /// </summary>
+    public partial class BookmarkEditor : Window
+    {
+        /// <summary>
+        /// Название закладки книги.
+        /// </summary>
+        public string BookmarkTitle;
+
+        public BookmarkEditor(string title)
+        {
+            InitializeComponent();
+            BookmarkTitle = title;
+            TitleTextBox.Text = title;
+        }
+
+        private void TitleTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            SaveButton.IsEnabled = TitleTextBox.Text.Any() && TitleTextBox.Text != BookmarkTitle;
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            BookmarkTitle = TitleTextBox.Text;
+            DialogResult = true;
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
