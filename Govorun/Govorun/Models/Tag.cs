e9d0851b45850b699c767d2f53776f2c1011af06
Геннадий <gemiyur@live.ru>
2025-06-25 11:06:32 +0300

    Классс Tag/

---
+++ Govorun/Govorun/Models/Tag.cs
@@ -0,0 +1,27 @@
+﻿namespace Govorun.Models;
+
+/// <summary>
+/// Класс тега книги.
+/// </summary>
+public class Tag : BaseModel
+{
+    /// <summary>
+    /// Идентификатор тега книги.
+    /// </summary>
+    public int TagId { get; set; }
+
+    private string title = string.Empty;
+
+    /// <summary>
+    /// Название тега книги.
+    /// </summary>
+    public string Title
+    {
+        get => title;
+        set
+        {
+            title = value ?? string.Empty;
+            OnPropertyChanged("Title");
+        }
+    }
+}
