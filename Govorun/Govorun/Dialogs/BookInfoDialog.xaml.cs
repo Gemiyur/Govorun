9d0ca29a7d1875b2762581365be7f6df105e66ee
Геннадий <gemiyur@live.ru>
2025-06-30 11:33:27 +0300

    Изменил окно "О книге".

--- Govorun/Govorun/Dialogs/BookInfoDialog.xaml.cs
+++ Govorun/Govorun/Dialogs/BookInfoDialog.xaml.cs
@@ -11,11 +11,40 @@ public partial class BookInfoDialog : Window
     public BookInfoDialog(Book book)
     {
         InitializeComponent();
-        TitleTextBlock.FontSize = FontSize + 2;
         AuthorsTextBlock.Text = book.AuthorNamesFirstLast;
+        TitleTextBlock.FontSize = FontSize + 2;
         TitleTextBlock.Text = book.Title;
-        LectorTextBlock.Text = book.Lector;
+        if (book.Cycle != null)
+        {
+            CycleTitleTextBlock.Text = book.Cycle.Title;
+            if (book.CyclePart.Length > 0)
+                CycleNumberTextBlock.Text = book.CyclePart;
+            else
+                CycleNumberStackPanel.Visibility = Visibility.Collapsed;
+        }
+        else
+        {
+            CycleGrid.Visibility = Visibility.Collapsed;
+        }
+        LectorTextBlock.Text = book.Lector.Length > 0 ? book.Lector : "(не указано)";
         DurationTextBlock.Text = book.DurationText;
+        if (book.Translator.Length > 0)
+        {
+            TranslatorTextBlock.Text = book.Translator;
+        }
+        else
+        {
+            TranslatorStackPanel.Visibility = Visibility.Collapsed;
+        }
+        if (book.Tags.Count > 0)
+        {
+            TagsTextBlock.Text =
+                App.ListToString(book.Tags, "; ", x => ((Tag)x).Title, StringComparer.CurrentCultureIgnoreCase);
+        }
+        else
+        {
+            TagsGrid.Visibility = Visibility.Collapsed;
+        }
         AnnotationTextBox.Text = book.Annotation;
         FileTextBox.Text = book.FileName;
     }
