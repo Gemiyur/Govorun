a6488f5cc2aa339489a079a44d35e0b4c3b0172b
Геннадий <gemiyur@live.ru>
2025-07-11 11:06:45 +0300

    Параметры для сохранения положения окон.

--- Govorun/Govorun/Properties/Settings.Designer.cs
+++ Govorun/Govorun/Properties/Settings.Designer.cs
@@ -94,5 +94,125 @@ namespace Govorun.Properties {
                 this["NavAuthorFullName"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool SaveBookWindowsLocation {
+            get {
+                return ((bool)(this["SaveBookWindowsLocation"]));
+            }
+            set {
+                this["SaveBookWindowsLocation"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0, 0")]
+        public global::System.Drawing.Point BookInfoPos {
+            get {
+                return ((global::System.Drawing.Point)(this["BookInfoPos"]));
+            }
+            set {
+                this["BookInfoPos"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0, 0")]
+        public global::System.Drawing.Size BookInfoSize {
+            get {
+                return ((global::System.Drawing.Size)(this["BookInfoSize"]));
+            }
+            set {
+                this["BookInfoSize"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0, 0")]
+        public global::System.Drawing.Point BookmarksPos {
+            get {
+                return ((global::System.Drawing.Point)(this["BookmarksPos"]));
+            }
+            set {
+                this["BookmarksPos"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0, 0")]
+        public global::System.Drawing.Size BookmarksSize {
+            get {
+                return ((global::System.Drawing.Size)(this["BookmarksSize"]));
+            }
+            set {
+                this["BookmarksSize"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0, 0")]
+        public global::System.Drawing.Point ChaptersPos {
+            get {
+                return ((global::System.Drawing.Point)(this["ChaptersPos"]));
+            }
+            set {
+                this["ChaptersPos"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0, 0")]
+        public global::System.Drawing.Size ChaptersSize {
+            get {
+                return ((global::System.Drawing.Size)(this["ChaptersSize"]));
+            }
+            set {
+                this["ChaptersSize"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool SaveMainWindowLocation {
+            get {
+                return ((bool)(this["SaveMainWindowLocation"]));
+            }
+            set {
+                this["SaveMainWindowLocation"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0, 0")]
+        public global::System.Drawing.Point MainWindowPos {
+            get {
+                return ((global::System.Drawing.Point)(this["MainWindowPos"]));
+            }
+            set {
+                this["MainWindowPos"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0, 0")]
+        public global::System.Drawing.Size MainWindowSize {
+            get {
+                return ((global::System.Drawing.Size)(this["MainWindowSize"]));
+            }
+            set {
+                this["MainWindowSize"] = value;
+            }
+        }
     }
 }
