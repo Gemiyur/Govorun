a46c16b3b6c1e6ac3994cd4cadeaa8f71b3fc6f0
Геннадий <gemiyur@live.ru>
2025-07-11 11:50:40 +0300

    Настройки сохранения положения окон.

--- Govorun/Govorun/Dialogs/SettingsDialog.xaml.cs
+++ Govorun/Govorun/Dialogs/SettingsDialog.xaml.cs
@@ -15,6 +15,8 @@ public partial class SettingsDialog : Window
         InitializeComponent();
         LoadLastBookCheckBox.IsChecked = Properties.Settings.Default.LoadLastBook;
         NavAuthorFullNameCheckBox.IsChecked = Properties.Settings.Default.NavAuthorFullName;
+        SaveBookWindowsLocationCheckBox.IsChecked = Properties.Settings.Default.SaveBookWindowsLocation;
+        SaveMainWindowLocationCheckBox.IsChecked = Properties.Settings.Default.SaveMainWindowLocation;
 #if DEBUG
         DbNameTextBox.Text = Properties.Settings.Default.DebugDbName;
 #else
@@ -43,6 +45,22 @@ public partial class SettingsDialog : Window
         Properties.Settings.Default.LoadLastBook = LoadLastBookCheckBox.IsChecked == true;
         Properties.Settings.Default.NavAuthorFullName = NavAuthorFullNameCheckBox.IsChecked == true;
         App.GetMainWindow().CheckAuthorsNameFormat();
+        Properties.Settings.Default.SaveBookWindowsLocation = SaveBookWindowsLocationCheckBox.IsChecked == true;
+        if (!Properties.Settings.Default.SaveBookWindowsLocation)
+        {
+            Properties.Settings.Default.BookInfoPos = new System.Drawing.Point(0, 0);
+            Properties.Settings.Default.BookInfoSize = new System.Drawing.Size(0, 0);
+            Properties.Settings.Default.BookmarksPos = new System.Drawing.Point(0, 0);
+            Properties.Settings.Default.BookmarksSize = new System.Drawing.Size(0, 0);
+            Properties.Settings.Default.ChaptersPos = new System.Drawing.Point(0, 0);
+            Properties.Settings.Default.ChaptersSize = new System.Drawing.Size(0, 0);
+        }
+        Properties.Settings.Default.SaveMainWindowLocation = SaveMainWindowLocationCheckBox.IsChecked == true;
+        if (!Properties.Settings.Default.SaveMainWindowLocation)
+        {
+            Properties.Settings.Default.MainWindowPos = new System.Drawing.Point(0, 0);
+            Properties.Settings.Default.MainWindowSize = new System.Drawing.Size(0, 0);
+        }
         if (DbNameChanged)
         {
 #if DEBUG
