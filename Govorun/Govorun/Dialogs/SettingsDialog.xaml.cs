585971b0120a3f5f1accede98bf08ec3932479b8
Геннадий <gemiyur@live.ru>
2025-11-23 13:04:52 +0300

    Перенёс проверку библиотеки в окно настроек.

--- Govorun/Govorun/Dialogs/SettingsDialog.xaml.cs
+++ Govorun/Govorun/Dialogs/SettingsDialog.xaml.cs
@@ -49,7 +49,18 @@ public partial class SettingsDialog : Window
 
     private void CheckLibraryButton_Click(object sender, RoutedEventArgs e)
     {
-
+        //ActionTextBlock.Text = "Проверка библиотеки...";
+        //ActionStatusBarItem.Visibility = Visibility.Visible;
+        var books = Library.Books.FindAll(
+            x => !x.FileExists).OrderBy(x => x.Title, StringComparer.CurrentCultureIgnoreCase);
+        //ActionStatusBarItem.Visibility = Visibility.Collapsed;
+        if (!books.Any())
+        {
+            MessageBox.Show("Проверка библиотеки выполнена.\nПроблем не обнаружено.", Title);
+            return;
+        }
+        var dialog = new CheckLibraryDialog(books) { Owner = this };
+        dialog.ShowDialog();
     }
 
     private void DbShrinkButton_Click(object sender, RoutedEventArgs e)
