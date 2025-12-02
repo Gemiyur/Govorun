044c118f20e26b5472fe88041ea8168b47278cfa
Геннадий <gemiyur@live.ru>
2025-12-02 13:40:55 +0300

    Класс Genre. Методы CopyTo и Clone.

--- Govorun/Govorun/Models/Genre.cs
+++ Govorun/Govorun/Models/Genre.cs
@@ -24,4 +24,30 @@ public class Genre : BaseModel
             OnPropertyChanged("Title");
         }
     }
+
+    /// <summary>
+    /// Копирует данные жанра в указанный жанр.
+    /// </summary>
+    /// <param name="genre">Жанр, в который копируются данные жанра.</param>
+    /// <remarks>
+    /// Копируются все данные жанра кроме идентификатора.
+    /// </remarks>
+    public void CopyTo(Genre genre)
+    {
+        genre.Title = Title;
+    }
+
+    /// <summary>
+    /// Создаёт и возвращает неполную копию жанра.
+    /// </summary>
+    /// <returns>Неполная копия жанра.</returns>
+    /// <remarks>
+    /// Копия содержит все данные жанра кроме идентификатора.
+    /// </remarks>
+    public Genre Clone()
+    {
+        var genre = new Genre();
+        CopyTo(genre);
+        return genre;
+    }
 }
